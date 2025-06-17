b30b0d67c3e2b16b4ae2c8520a7947da66c587d6
Borong <borongxu@outlook.com>
2025-06-17 20:35:09 +1000

    Update five_camera_capture.py

--- five_camera_capture.py
+++ five_camera_capture.py
@@ -16,15 +16,13 @@ ret5, frame_5 = camera_5.read()
 
 # Concatenate to get the final frame size
 black_frame = np.zeros_like(frame_1)
-frame1_height, frame1_width = frame_1.shape[:2]
+height, width = frame_1.shape[:2]
 row1 = np.concatenate((frame_1, frame_2, frame_3), axis=1)
 row2 = np.concatenate((frame_4, frame_5, black_frame), axis=1)
-sample_frame = np.concatenate((row1, row2), axis=0)
-height, width, channels = sample_frame.shape
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (width, height))
+out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (width * 3, height * 2))
 
 while True:
     ret1, frame_1 = camera_1.read()
@@ -33,7 +31,7 @@ while True:
     ret4, frame_4 = camera_4.read()
     ret5, frame_5 = camera_5.read()
     
-    black_frame = np.zeros((frame1_height, frame1_width, 3), dtype=np.uint8)
+    black_frame = np.zeros((height, width, 3), dtype=np.uint8)
     if not ret1:
         frame_1 = black_frame.copy()
     if not ret2:
@@ -46,17 +44,18 @@ while True:
         frame_5 = black_frame.copy()
     
     # Resize all frames to the size of frame_1
-    frame_2 = cv2.resize(frame_2, (frame1_width, frame1_height))
-    frame_3 = cv2.resize(frame_3, (frame1_width, frame1_height))
-    frame_4 = cv2.resize(frame_4, (frame1_width, frame1_height))
-    frame_5 = cv2.resize(frame_5, (frame1_width, frame1_height))
-    black_frame = cv2.resize(black_frame, (frame1_width, frame1_height))
+    frame_1 = cv2.resize(frame_1, (width, height))
+    frame_2 = cv2.resize(frame_2, (width, height))
+    frame_3 = cv2.resize(frame_3, (width, height))
+    frame_4 = cv2.resize(frame_4, (width, height))
+    frame_5 = cv2.resize(frame_5, (width, height))
+    black_frame = cv2.resize(black_frame, (width, height))
     
     row1 = np.concatenate((frame_1, frame_2, frame_3), axis=1)
     row2 = np.concatenate((frame_4, frame_5, black_frame), axis=1)
     frame = np.concatenate((row1, row2), axis=0)
 
-    cv2.imshow('Camera 1', frame)
+    cv2.imshow('Cameras', frame)
     out.write(frame)  # Write the frame to the video file
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
