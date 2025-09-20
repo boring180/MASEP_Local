3cb9a9b92124c7414d34d960578fe3de1ea7269f
Borong <borongxu@outlook.com>
2025-09-20 12:49:53 +0800

    Change variable name

--- Callibration/extrinsic.py
+++ Callibration/extrinsic.py
@@ -5,8 +5,6 @@ import os
 from get_points import get_points
 from settings_loader import settings
 
-
-
 def extrinsic_calibration(settings):
     if settings.same_pattern:
         if os.path.exists('chessboard_points/intrinsic_rets.npy') and os.path.exists('chessboard_points/intrinsic_object_points.npy') and os.path.exists('chessboard_points/intrinsic_image_points.npy') and os.path.exists('chessboard_points/intrinsic_shape.npy'):
@@ -41,19 +39,19 @@ def extrinsic_calibration(settings):
             pass
         else:
             stereo_objpoints = []
-            stereo_imgpoints1 = []
-            stereo_imgpoints2 = []
+            stereo_imgpoints_camera = []
+            stereo_imgpoints_center = []
             for i in range(len(objpoints)):
                 if rets[i, cameras.index(camera_name)] and rets[i, cameras.index(center_camera)]:
                     stereo_objpoints.append(objpoints[i, cameras.index(camera_name), :, :].astype(np.float32))
-                    stereo_imgpoints1.append(imgpoints[i, cameras.index(camera_name), :, :].astype(np.float32))
-                    stereo_imgpoints2.append(imgpoints[i, cameras.index(center_camera), :, :].astype(np.float32))
+                    stereo_imgpoints_camera.append(imgpoints[i, cameras.index(camera_name), :, :].astype(np.float32))
+                    stereo_imgpoints_center.append(imgpoints[i, cameras.index(center_camera), :, :].astype(np.float32))
             stereo_objpoints = np.array(stereo_objpoints)
-            stereo_imgpoints1 = np.array(stereo_imgpoints1)
-            stereo_imgpoints2 = np.array(stereo_imgpoints2)
+            stereo_imgpoints_camera = np.array(stereo_imgpoints_camera)
+            stereo_imgpoints_center = np.array(stereo_imgpoints_center)
             
             flags = (cv2.CALIB_FIX_INTRINSIC)
-            results = cv2.stereoCalibrate(objectPoints=stereo_objpoints, imagePoints1=stereo_imgpoints1, imagePoints2=stereo_imgpoints2,
+            results = cv2.stereoCalibrate(objectPoints=stereo_objpoints, imagePoints1=stereo_imgpoints_camera, imagePoints2=stereo_imgpoints_center,
                                         cameraMatrix1=mtxs[camera_name], distCoeffs1=dists[camera_name], cameraMatrix2=mtxs[center_camera], 
                                         distCoeffs2=dists[center_camera], imageSize=shape, 
                                         flags=flags)
@@ -65,8 +63,27 @@ def extrinsic_calibration(settings):
             print(f'{camera_name} has {len(stereo_objpoints)} successful images')
             with open(f'results/extrinsic_calibration.log', 'a') as f:
                 f.write(f'{camera_name} has {len(stereo_objpoints)} successful images\n')
+                
+            number_of_success_images = stereo_objpoints.shape[0]
+            reprojection_errors = []
+            for i in range(number_of_success_images):
+                ret_camera, tvecs_camera, rvecs_camera = cv2.solvePnP(stereo_objpoints[i, :, :], stereo_imgpoints_camera[i, :, :], mtxs[camera_name], dists[camera_name])
+                ret_center, tvecs_center, rvecs_center = cv2.solvePnP(stereo_objpoints[i, :, :], stereo_imgpoints_center[i, :, :], mtxs[center_camera], dists[center_camera])
+                if ret_camera == True and ret_center == True:
+                    error = tvecs_camera - tvecs_center
+                    reprojection_errors.append(error)
+            
+            mean_reprojection_error = np.mean(reprojection_errors, axis=0)
+            std_reprojection_error = np.std(reprojection_errors, axis=0)
+            print(f'{camera_name} has reprojection error mean: {mean_reprojection_error}')
+            print(f'{camera_name} has reprojection error std: {std_reprojection_error}')
+            with open(f'results/extrinsic_calibration.log', 'a') as f:
+                f.write(f'{camera_name} has reprojection error mean: {mean_reprojection_error}\n')
+                f.write(f'{camera_name} has reprojection error std: {std_reprojection_error}\n')
+                    
             
             
+    
         print(f'{camera_name} transformation matrix: {transformation_matrix}')
         with open(f'results/extrinsic_calibration.log', 'a') as f:
             f.write(f'{camera_name} transformation matrix: {transformation_matrix}\n')
