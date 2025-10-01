eaa61ec3c1a344f5c04f7cb1f7687f87efe66ef2
Borong <borongxu@outlook.com>
2025-10-01 13:06:32 +0800

    Update code for point matching algorithm

--- Callibration/extrinsic_calibration_charuco.py
+++ Callibration/extrinsic_calibration_charuco.py
@@ -8,6 +8,7 @@ import time
 import random
 import tqdm
 import pickle
+import scipy.spatial.transform.Rotation as R
 random.seed(time.time())
 
 from settings_loader import settings
@@ -18,7 +19,7 @@ from utils.frame_slicing import slicing_frame3_1, slicing_frame3_2
 class ExtrinsicCalibrationCharuco:
     def __init__(self, settings):
         self.dict = cv2.aruco.getPredefinedDictionary(getattr(cv2.aruco, settings.aruco_dict_external))
-        self.board = cv2.aruco.CharucoBoard((settings.pattern_size_external[0], settings.pattern_size_external[1]), settings.pattern_square_size_external, settings.marker_size_external, self.dict)
+        self.board = cv2.aruco.CharucoBoard((settings.pattern_size_external[1], settings.pattern_size_external[0]), settings.pattern_square_size_external, settings.marker_size_external, self.dict)
         self.parameters = cv2.aruco.DetectorParameters()
         self.detector = cv2.aruco.ArucoDetector(self.dict, self.parameters)
         
@@ -44,54 +45,162 @@ class ExtrinsicCalibrationCharuco:
             frame_points = {}
             image = cv2.imread(image_path)
             frames = slicing_frame3_1(image)
-            for frame in frames:
-                camera_name = self.cameras[0]
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            for i in range(len(frames)):
+                camera_name = self.cameras[i]
+                gray = cv2.cvtColor(frames[i], cv2.COLOR_BGR2GRAY)
                 corners, ids, rejected = self.detector.detectMarkers(gray)
                 if ids is None:
                     frame_points[camera_name] = None
                     continue
                 
                 ret, corners, ids = cv2.aruco.interpolateCornersCharuco(corners, ids, gray, self.board)
-                if not ret:
+                number_of_corners = len(corners)
+
+                # if ret:
+                #     name = f"{image_path.split('/')[-1].split('.')[0]}_{camera_name}"
+                #     self.draw_points(gray, corners, ids, name)
+                
+                if not ret or number_of_corners < 6:
                     frame_points[camera_name] = None
                     continue
                 
+                
                 ret, rvec, tvec = cv2.aruco.estimatePoseCharucoBoard(corners, ids, self.board, self.camera_mtx[camera_name], self.camera_dist[camera_name], None, None, useExtrinsicGuess=False)
-                frame_points[camera_name] = (rvec, tvec)
+                # Change to board to camera transformation
+                tvec = -tvec
+                
+                frame_points[camera_name] = (rvec, tvec, number_of_corners)
                 
             self.camera_points.append(frame_points)
             
-    def calibrate(self, camera_name):
+    def calibrate(self, camera_name, weighted = False):
         camera_Rs = []
         camera_Ts = []
+        camera_number_of_corners = []
         
         center_Rs = []
         center_Ts = []
+        center_number_of_corners = []
+        
         
         for frame_points in self.camera_points:
-            if frame_points[camera_name] is not None:
-                camera_Rs.append(frame_points[camera_name][0])
-                camera_Ts.append(frame_points[camera_name][1])
-            if frame_points[self.center_camera] is not None:
-                center_Rs.append(frame_points[self.center_camera][0])
-                center_Ts.append(frame_points[self.center_camera][1])
+            if frame_points[camera_name] is not None and frame_points[self.center_camera] is not None:
+                camera_Rs.append(frame_points[camera_name][0][:, 0])
+                camera_Ts.append(frame_points[camera_name][1][:, 0])
+                camera_number_of_corners.append(frame_points[camera_name][2])
+                center_Rs.append(frame_points[self.center_camera][0][:, 0])
+                center_Ts.append(frame_points[self.center_camera][1][:, 0])
+                center_number_of_corners.append(frame_points[self.center_camera][2])
         
         camera_Rs = np.array(camera_Rs)
         camera_Ts = np.array(camera_Ts)
         center_Rs = np.array(center_Rs)
         center_Ts = np.array(center_Ts)
         
+        if weighted:
+            pass
+        else:
+            weight = np.ones(camera_Rs.shape[0])
         
+        print(f'{camera_name} has {camera_Ts.shape[0]} points')
+        with open(f'results/extrinsic_calibration.log', 'a') as f:
+            f.write(f'{camera_name} has {camera_Ts.shape[0]} points\n')
+        
+        R, t, rmsd = self.point_cloud_matching(camera_Ts, center_Ts, weight)
+        print(f'{camera_name} has RMSD: {rmsd}')
+        with open(f'results/extrinsic_calibration.log', 'a') as f:
+            f.write(f'{camera_name} has RMSD: {rmsd}\n')
+        
+        self.camera_extrinsic[camera_name] = np.eye(4)
+        self.camera_extrinsic[camera_name][:3, :3] = R.as_matrix()
+        self.camera_extrinsic[camera_name][:3, 3] = t
+        
+        pickle.dump(self.camera_extrinsic, open(f'results/extrinsic_{camera_name}.pkl', 'wb'))
+    
+    def ransac(self):
         pass
     
+    # P, Q: (N, 3)
+    # weight: (N,)
+    def point_cloud_matching(self, P, Q, weight):
+        assert P.shape == Q.shape
+        assert weight.shape == P.shape[0]
+
+        transformation = (Q - P) * weight[:, np.newaxis]
+        transformation = transformation.sum(axis=0) / weight.sum()
+        
+        P_prime = P + transformation
+        
+        R, rssd, sse = R.align_vectors(Q, P_prime, weights=weight)
+        return R, transformation, rmsd
+    
     def evaluate(self):
-        pass
+        fig = plt.figure(figsize=(15, 10))
+        self.visualize_camera_location(settings, fig, 45, 0, 0, i=1)
+        self.visualize_camera_location(settings, fig, elev=45, azim=45, roll=0, i=2)
+        self.visualize_camera_location(settings, fig, elev=-90, azim=90, roll=0, i=3)
+        plt.savefig('results/visualize_ext.png')
+        
+        
+    def visualize_camera_location(self, settings, fig, elev, azim, roll, i):
+        transformation_matrices = {}
+        for camera_name in settings.cameras:
+            transformation_matrix = pickle.load(open(f'results/extrinsic_{camera_name}.pkl', 'rb'))
+            transformation_matrices[camera_name] = transformation_matrix
+                
+        ax = fig.add_subplot(1, 3, i, projection='3d', elev=elev, azim=azim, roll=roll)
+        ax.set_box_aspect([1,1,1])
+        length = 0.5
+        width = 0.5
+        height = 0.5
+        
+        x = [-length/2, length/2, length/2, -length/2]
+        y = [-width/2, -width/2, width/2, width/2]
+        for i in range(4):
+            ax.plot([x[i], x[i]], [y[i], y[i]], [0, height], 'b-')
+            
+        x = [-length/2, length/2, length/2, -length/2]
+        z = [0, 0, height, height]
+        for i in range(4):
+            ax.plot([x[i], x[i]], [-width/2, width/2], [z[i], z[i]], 'b-')
+            
+        y = [-length/2, length/2, length/2, -length/2]
+        z = [0, 0, height, height]
+        for i in range(4):
+            ax.plot([-length/2, length/2], [y[i], y[i]], [z[i], z[i]], 'b-')
+        
+        for camera_name in settings.cameras:
+            colors = ['red', 'green', 'blue']
+            axis = np.eye(3) * settings.pattern_square_size_external
+            for i in range(3):
+                orientation = transformation_matrices[camera_name][:3, :3] @ axis[i, :]
+                x = transformation_matrices[camera_name][0, 3]
+                y = transformation_matrices[camera_name][1, 3]
+                z = transformation_matrices[camera_name][2, 3]
+                u = orientation[0]
+                v = orientation[1]
+                w = orientation[2]
+                ax.quiver(x, y, z, u, v, w, color = colors[i], length=1)
+                
+                if i == 0:
+                    ax.text(x, y, z, camera_name, color = 'black')
+            
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_zlabel('Z')
+    
+    def draw_points(self, gray, corners, ids, name):
+        for i in range(len(corners)):
+            cv2.circle(gray, (int(corners[i][0][0]), int(corners[i][0][1])), 5, (0, 0, 255), -1)
+        os.makedirs('debug_frames', exist_ok=True)
+        cv2.imwrite(f'debug_frames/{name}.jpg', gray)
+        
 
 ### ----------------------------- Main function ----------------------------- ###
 def main():
     extrinsic_calibration_charuco = ExtrinsicCalibrationCharuco(settings)
     extrinsic_calibration_charuco.get_camera_points()
+    extrinsic_calibration_charuco.calibrate('cam0')
 
 if __name__ == '__main__':
     main()
